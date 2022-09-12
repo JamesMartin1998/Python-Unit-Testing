7d5d04cbc731e2f9d5a0a8833a3abedaea98e86b
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-09-12 13:38:45 +0000

    add test_apply_extenstion

--- test_student.py
+++ test_student.py
@@ -1,21 +1,39 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 class TestStudent(unittest.TestCase):
-    def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
 
-    
-    def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
+
+    def tearDown(self):
+        print("tearDown")
+
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
 
     def test_email(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.email, f"john.doe@email.com")
+        print("test_email")
+        self.assertEqual(self.student.email, f"john.doe@email.com")
+
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+
+    def test_apply_extension(self):
+        print("test_apply_extension")
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
 
 if __name__ == "__main__":
     unittest.main()
